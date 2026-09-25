90dd116abfcc4237fe040ed707a4d725d3fc6274
Raul Frasacco <rfrabae0511@g.educaand.es>
2026-09-25 14:54:14 +0200

    primeros pasos sobre la sintaxis

--- web/aplicacion/pruebas/basicas.php
+++ web/aplicacion/pruebas/basicas.php
@@ -1,26 +1,28 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
-
-
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
-inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+inicioCuerpo("Pruebas basicas");
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
 
-//vista
-function cabecera() 
-{}
 
+
+
+//vista
+function cabecera() {}
 //vista
 function cuerpo()
 {
 ?>
-    <br><br>
-    <a href="./aplicacion/pruebas/index.php">Ir a Prubas</a>
+    <br><br>esto es html
+   <?php
+        echo "gshgfgksj";
+   ?>
+
 <?php
 }
--- web/aplicacion/pruebas/index.php
+++ web/aplicacion/pruebas/index.php
@@ -1,26 +1,26 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
-
-
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
 inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
 
-//vista
-function cabecera() 
-{}
 
+
+//vista
+function cabecera() {}
 //vista
 function cuerpo()
 {
 ?>
     <br><br>
-    <a href="./aplicacion/pruebas/index.php">Ir a Prubas</a>
+    Hola, estás en Index.php
+    <br><br>
+    <a href="basicas.php">Funcionamiento basico</a>
 <?php
 }
